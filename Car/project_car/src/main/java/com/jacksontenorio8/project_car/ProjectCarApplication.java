69a4409cb2c990a546db709444434561015dabc2
Jackson <jacksontenorio8@gmail.com>
2021-04-18 08:03:58 -0300

    Project Car

---
+++ Car/project_car/src/main/java/com/jacksontenorio8/project_car/ProjectCarApplication.java
@@ -0,0 +1,13 @@
+package com.jacksontenorio8.project_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCarApplication.class, args);
+	}
+
+}
---
+++ Car/project_car/src/test/java/com/jacksontenorio8/project_car/ProjectCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksontenorio8.project_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
